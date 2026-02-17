0faddddaecf012a50323c2fe513d300ce0ee0a2c
ta0118-droid <ta0118@srmist.edu.in>
2026-02-17 10:11:43 +0530

    [Tanishi Agrawal] Palindrome Check Using String Reverse

---
+++ src/UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+public class UseCase3PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC3.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        String input = "radar";   // Hardcoded string
+        String reversed = "";
+
+        // Build reversed string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reversed);
+
+        if (input.equals(reversed)) {
+            System.out.println("Result: The string is a palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
+    }
+}
