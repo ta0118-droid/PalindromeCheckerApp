055187f991a93b5a44180a9d8122516b1693edba
ta0118-droid <ta0118@srmist.edu.in>
2026-02-17 10:08:40 +0530

    [Tanishi Agrawal] Displaying welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {
+    /**
+     * Application entry point.
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("  Welcome to Palindrome Checker");
+        System.out.println("  Version 1.0");
+        System.out.println("===================================");
+        System.out.println("Application started successfully.");
+
+    }
+}
