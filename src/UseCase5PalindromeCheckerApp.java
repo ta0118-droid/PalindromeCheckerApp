2987aa6471eaf543c3826c0d53d79696f1d32938
ta0118-droid <ta0118@srmist.edu.in>
2026-02-17 10:27:08 +0530

    [Tanishi Agrawal] Stack-Based Palindrome Checker

---
+++ src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize the input string.
+        String input = "noon";
+
+        // Create a Stack to store characters.
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character of the string into the stack.
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Iterate again through original string.
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
